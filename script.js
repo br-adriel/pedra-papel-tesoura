6dee313bf9073a4a4ea5a9c2b6687466ceb69cf9
Adriel <adriel.fsantos@outlook.com>
2021-12-28 18:40:47 -0300

    Implementa função jogarPartida

--- script.js
+++ script.js
@@ -9,3 +9,39 @@ function computerPlay() {
       return "tesoura";
   }
 }
+
+function jogarPartida(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  switch (computerSelection) {
+    case "pedra":
+      if (playerSelection === "papel") {
+        return 1;
+      } else if (playerSelection === "tesoura") {
+        return 2;
+      } else if (playerSelection === "pedra") {
+        return 0;
+      } else {
+        return -1;
+      }
+    case "papel":
+      if (playerSelection === "tesoura") {
+        return 1;
+      } else if (playerSelection === "pedra") {
+        return 2;
+      } else if (playerSelection === "papel") {
+        return 0;
+      } else {
+        return -1;
+      }
+    case "tesoura":
+      if (playerSelection === "pedra") {
+        return 1;
+      } else if (playerSelection === "papel") {
+        return 2;
+      } else if (playerSelection === "tesoura") {
+        return 0;
+      } else {
+        return -1;
+      }
+  }
+}
